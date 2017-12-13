cd2774dc83cbe160afdc08d01f27c58ba20872f1
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-12-13 17:15:10 +0100

    create pdf-viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  pdfSrc: string;
+  error: any;
+  page: number = 1;
+  zoom: number = 1.0;
+  originalSize: boolean = false;
+  pdf: any;
+  renderText: boolean = true;
+  progressData;
+  isLoaded: boolean = false;
+  stickToPage = false;
+  showAll: boolean = false;
+  autoresize: boolean = true;
+  isPdfViewerVisible: boolean = false;
+
+  incrementPage(amount: number) {
+    this.page += amount;
+  }
+
+  incrementZoom(amount: number) {
+    this.zoom += amount;
+  }
+
+  onFileSelected() {
+    const file: any = document.querySelector('#file');
+
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.pdfSrc = e.target.result;
+      };
+
+      reader.readAsArrayBuffer(file.files[0]);
+    }
+  }
+
+  afterLoadComplete(pdf) {
+    this.pdf = pdf;
+    this.isLoaded = true;
+  }
+
+  onError(error: any) {
+    this.error = error;
+  }
+
+  onProgress(progressData) {
+    this.progressData = progressData;
+    this.isLoaded = false;
+    this.error = null; // clear error
+  }
+
+  getInt(value: number): number {
+    return Math.round(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PdfViewerModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
